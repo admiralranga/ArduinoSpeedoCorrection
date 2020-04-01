a622d10590d2004dba5c232183b25bed96c38117
admiralranga <harry.king@treasurekindy.com.au>
2020-04-01 15:36:02 +0800

    changed to 2hz minium frequency

--- ArduinoSpeedoCorrection.ino
+++ ArduinoSpeedoCorrection.ino
@@ -7,9 +7,11 @@
 //
 //Modified to accept a digital signal on pin 3 rather than a ground based pulse - requires 10K pulldown
 
-const int debounce = 2500; // this may need to reduce for TD5 - there are some scenarios where a signal may only be 2.5ms
-const int speedometerPin = 10;
-const int sensorPin = 3;
+const int debounce = 2000; // this may need to reduce for TD5 - there are some scenarios where a signal may only be 2.5ms
+const int speedometerPin = 7;
+const int sensorPin = 2;
+const int ledPin = 17;
+
 int pulseState = HIGH;
 volatile unsigned long currentMicros = 0; // we treat these as volatile because they can be changed by our interrupt
 volatile unsigned long previousMicros = 0;
@@ -18,14 +20,15 @@ volatile unsigned long previousSpeed = 0;
 volatile unsigned long interval = 0;
 volatile unsigned long oneHertz = 0;
 unsigned long modInterval = 0;
-float calFactor = 1.1;   // decrease to slow down speedometer  (use 1/ratio to get calfactors for speedup)
+float calFactor = 1.05;   // decrease to slow down speedometer  (use 1/ratio to get calfactors for speedup)
 // calFactor of 1 makes no change to speedometer
 
 void setup()
 {
-  Serial.begin(115200); //for debug - needs to be a high baud otherwise we delay the actuial signal measurement
+  //Serial.begin(115200); //for debug - needs to be a high baud otherwise we delay the actuial signal measurement
   pinMode(speedometerPin, OUTPUT);
   pinMode(sensorPin, INPUT);
+  pinMode(ledPin, OUTPUT);
   digitalWrite (sensorPin, LOW); // pin 10 is pulled low by a 10k resistor.
   attachInterrupt (1, iSr, RISING); // when we detect a pulse call our interrupt code
 }
@@ -36,24 +39,27 @@ void loop()
   modInterval = interval;
   interrupts();
   currentMicros = micros();
-  if (currentMicros - previousSpeed < 300000) // slowest moving signal from SLABS should be 454micros peak to peak, so if we are seeing much more than half of this then we must be stationary
+  if (currentMicros - previousSpeed < 150000) // halved 2hz slowest moving signal from SLABS should be 454micros peak to peak, so if we are seeing much more than half of this then we must be stationary
   { // 1mph = 2.2hz ~230ms, so anything above this means we are stationary
     if (currentMicros - previousMicros > ((modInterval / 2) / calFactor)) //divided by 2 because modInterval is one whole wavelength, we need to do a high low transition for half the time each of this.
     { previousMicros = currentMicros;
       if (pulseState == LOW) pulseState = HIGH; else pulseState = LOW;
       Serial.println(micros()); //debug out
+      Serial.println(pulseState); //debug out
       digitalWrite(speedometerPin, pulseState); // output our corrected VSS
+      digitalWrite(ledPin, pulseState); // blink the LED
       oneHertz = currentMicros;
     }
   }
 
-  if (currentMicros - oneHertz > 500000) { // if we are stationary output a 1hz signal
+  if (currentMicros - oneHertz > 250000) { //halved 2hz if we are stationary output a 1hz signal
     if (pulseState == LOW) {
       pulseState = HIGH;
     } else {
       pulseState = LOW;
     }
     digitalWrite(speedometerPin, pulseState); // output our one hz VSS
+    digitalWrite(ledPin, pulseState); // blink the LED
     oneHertz = currentMicros;
   }
 
